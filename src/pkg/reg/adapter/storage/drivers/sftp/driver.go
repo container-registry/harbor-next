2d32ef5297f49c6b6b8ff2d80f7545e29ebb13ea
Maksym Trofimenko <maksym@container-registry.com>
2024-08-10 18:55:22 +0100

    fix list path

--- src/pkg/reg/adapter/storage/drivers/sftp/driver.go
+++ src/pkg/reg/adapter/storage/drivers/sftp/driver.go
@@ -242,7 +242,7 @@ func (d *Driver) Health(_ context.Context) error {
 		return err
 	}
 	defer cl()
-	_, err = client.ReadDir("")
+	_, err = client.ReadDir("/")
 	return err
 }
 
--- src/pkg/reg/adapter/storage/sftp_factory.go
+++ src/pkg/reg/adapter/storage/sftp_factory.go
@@ -2,7 +2,6 @@ package storage
 
 import (
 	"context"
-	"github.com/davecgh/go-spew/spew"
 	"github.com/docker/distribution/registry/storage"
 	"github.com/docker/libtrust"
 	"github.com/goharbor/harbor/src/lib/log"
@@ -26,7 +25,6 @@ type sftpFactory struct {
 // Create ...
 func (f *sftpFactory) Create(r *model.Registry) (regadapter.Adapter, error) {
 
-	spew.Dump("FACTORY CREATE", r)
 	driver, err := sftpdriver.New(r)
 	if err != nil {
 		return nil, err
