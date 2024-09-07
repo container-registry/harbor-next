73840f23bee2270bbcfc1b75f61d1ec0a01c3d84
Maksym Trofimenko <maksym@container-registry.com>
2024-09-07 15:58:34 +0100

    debug health check s3 error

--- src/pkg/reg/adapter/storage/drivers/s3/driver.go
+++ src/pkg/reg/adapter/storage/drivers/s3/driver.go
@@ -2,6 +2,7 @@ package s3
 
 import (
 	"context"
+	"github.com/davecgh/go-spew/spew"
 	"github.com/docker/distribution/registry/storage/driver/s3-aws"
 	"github.com/goharbor/harbor/src/pkg/reg/adapter/storage/health"
 )
@@ -12,6 +13,7 @@ type Driver struct {
 
 func (d Driver) Health(ctx context.Context) error {
 	_, err := d.List(ctx, "")
+	spew.Dump(err)
 	return err
 }
 
