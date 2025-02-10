84e397e78d90f327bd74b9bcbce9a23b57494fb0
bupd <bupdprasanth@gmail.com>
2025-02-10 07:28:24 +0530

    add vulnerability report generation
    Signed-off-by: bupd <bupdprasanth@gmail.com>

--- .dagger/tests.go
+++ .dagger/tests.go
@@ -21,8 +21,9 @@ func (m *Harbor) Lint(ctx context.Context) (string, error) {
 	return m.lint(ctx).WithExec([]string{"golangci-lint", "-v", "run", "--timeout=10m"}).Stderr(ctx)
 }
 
-func (m *Harbor) lint(_ context.Context) *dagger.Container {
+func (m *Harbor) lint(ctx context.Context) *dagger.Container {
 	fmt.Println("👀 Running linter.")
+	m.Source = m.genAPIs(ctx)
 	linter := dag.Container().
 		From("golangci/golangci-lint:"+GOLANGCILINT_VERSION+"-alpine").
 		WithMountedCache("/lint-cache", dag.CacheVolume("/lint-cache")).
@@ -34,14 +35,24 @@ func (m *Harbor) lint(_ context.Context) *dagger.Container {
 	return linter
 }
 
-// Check vulnerabilities in go-code
-func (m *Harbor) GoVulnCheck(ctx context.Context) (string, error) {
-	fmt.Println("👀 Running Go vulnerabilities check")
+func (m *Harbor) goVulnCheck(ctx context.Context) *dagger.Container {
 	m.Source = m.genAPIs(ctx)
 	return dag.Container().
 		From("golang:alpine").
 		WithMountedDirectory("/harbor", m.Source).
 		WithWorkdir("/harbor/src").
 		WithExec([]string{"go", "install", "golang.org/x/vuln/cmd/govulncheck@latest"}).
-		WithExec([]string{"/go/bin/govulncheck", "-show", "verbose", "./..."}).Stdout(ctx)
+		WithEntrypoint([]string{"/go/bin/govulncheck"})
+}
+
+// Check vulnerabilities in go-code
+func (m *Harbor) GoVulnCheck(ctx context.Context) (string, error) {
+	fmt.Println("👀 Running Go vulnerabilities check")
+	return m.goVulnCheck(ctx).WithExec([]string{"govulncheck", "-show", "verbose", "./..."}).Stdout(ctx)
+}
+
+// Generate Vulnerability Report in sarif format for github-actions
+func (m *Harbor) GoVulnCheckReport(ctx context.Context) (string, error) {
+	fmt.Println("👀 Generating Vulnerability Report")
+	return m.goVulnCheck(ctx).WithExec([]string{"govulncheck", "-format", "sarif", "./..."}).Stdout(ctx)
 }
