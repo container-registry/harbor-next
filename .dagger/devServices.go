ffc1efbc57aeb1ff165d6e1bac64dd31fbe30c1c
bupd <bupdprasanth@gmail.com>
2025-02-21 09:39:22 +0530

    update db and redis service
    Signed-off-by: bupd <bupdprasanth@gmail.com>

--- .dagger/devServices.go
+++ .dagger/devServices.go
@@ -7,8 +7,8 @@ import (
 
 // registryctl: Registry controller for interacting with the registry.
 // core: The business logic service that relies on the database, registry, and Redis.
-// portal: The user-facing portal can be started after core.
 // jobservice: Background jobs, which require the core service to be available.
+// portal: The user-facing portal can be started after core.
 // proxy: The last service to start as it routes traffic to all the other services.
 
 func (m *Harbor) RegistryCtlService(ctx context.Context) *dagger.Service {
@@ -27,7 +27,7 @@ func (m *Harbor) RegistryCtlService(ctx context.Context) *dagger.Service {
 	return regCtl
 }
 
-func (m *Harbor) DbService() *dagger.Service {
+func (m *Harbor) DbService(ctx context.Context) *dagger.Service {
 	postgres := dag.Container().From("goharbor/harbor-db:dev").
 		WithExposedPort(5432).
 		WithEnvVariable("POSTGRES_PASSWORD", "root123").
@@ -35,7 +35,7 @@ func (m *Harbor) DbService() *dagger.Service {
 	return postgres
 }
 
-func (m *Harbor) RedisService() *dagger.Service {
+func (m *Harbor) RedisService(ctx context.Context) *dagger.Service {
 	return dag.Container().
 		From("goharbor/redis-photon:dev").
 		WithExposedPort(6379).
@@ -46,9 +46,10 @@ func (m *Harbor) RegistryService(ctx context.Context) *dagger.Service {
 	regConfigDir := m.Source.Directory(".dagger/config/registry")
 
 	// 5001 is can be used to debug according to config
-	reg := m.buildRegistry(ctx, DEV_PLATFORM).
+	// reg := m.buildRegistry(ctx, DEV_PLATFORM).
+	reg := m.BuildImage(ctx, DEV_PLATFORM, "registry", DEV_VERSION).
 		WithMountedDirectory("/etc/registry", regConfigDir).
-		WithServiceBinding("redis", m.RedisService()).
+		WithServiceBinding("redis", m.RedisService(ctx)).
 		WithExposedPort(5000).
 		// WithExposedPort(5001).
 		AsService()
