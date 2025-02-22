44dcd4be441e9348053eadab59dd3d3c1cbd676f
bupd <bupdprasanth@gmail.com>
2025-02-22 08:16:40 +0530

    add run dev
    Signed-off-by: bupd <bupdprasanth@gmail.com>

--- .dagger/dev.go
+++ .dagger/dev.go
@@ -6,14 +6,50 @@ import (
 )
 
 // Run Harbor inside Dagger
-func (m *Harbor) RunDev(
-	ctx context.Context,
-	// +optional
-	// +defaultPath="."
-	source *dagger.Directory,
-) (*dagger.Service, error) {
-	golang := dag.Container().
-		WithExposedPort(8080)
+func (m *Harbor) DevServer(ctx context.Context) (*dagger.Service, error) {
+	var err error
 
-	return golang.AsService(), nil
+	postgresSrv := m.PostgresService(ctx)
+	redisSrv := m.RedisService(ctx)
+	regSrv := m.RegistryService(ctx)
+	regCtlSrv := m.RegistryCtlService(ctx)
+	coreSrv := m.coreService(ctx)
+	jobSrv := m.JobService(ctx)
+	portalSrv := m.PortalService(ctx)
+	nginxSrv := m.NginxService(ctx)
+
+	_, err = postgresSrv.WithHostname("postgresql").Start(ctx)
+	if err != nil {
+		return nil, err
+	}
+	_, err = redisSrv.WithHostname("redis").Start(ctx)
+	if err != nil {
+		return nil, err
+	}
+	_, err = regSrv.WithHostname("registry").Start(ctx)
+	if err != nil {
+		return nil, err
+	}
+	_, err = regCtlSrv.WithHostname("registryctl").Start(ctx)
+	if err != nil {
+		return nil, err
+	}
+	_, err = coreSrv.WithHostname("core").Start(ctx)
+	if err != nil {
+		return nil, err
+	}
+	_, err = jobSrv.WithHostname("jobservice").Start(ctx)
+	if err != nil {
+		return nil, err
+	}
+	_, err = portalSrv.WithHostname("portal").Start(ctx)
+	if err != nil {
+		return nil, err
+	}
+	proxy, err := nginxSrv.WithHostname("proxy").Start(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	return proxy, nil
 }
