51f6e3b04961da9f1e90c0f3a9edf5f43817a508
bupd <bupdprasanth@gmail.com>
2025-03-05 22:07:24 +0530

    move constants to separate file
    Signed-off-by: bupd <bupdprasanth@gmail.com>

---
+++ .dagger/consts.go
@@ -0,0 +1,26 @@
+package main
+
+import "strings"
+
+const (
+	GOLANGCILINT_VERSION = "v1.61.0"
+	GO_VERSION           = "latest"
+	SYFT_VERSION         = "v1.9.0"
+	GORELEASER_VERSION   = "v2.3.2"
+	// version of registry for pulling the source code
+	REGISTRY_SRC_TAG = "v2.8.3"
+	// source of upstream distribution code
+	DISTRIBUTION_SRC = "https://github.com/distribution/distribution.git"
+	NPM_REGISTRY     = "https://registry.npmjs.org"
+	// trivy-adapter
+	TRIVYVERSION        = "v0.56.1"
+	TRIVYADAPTERVERSION = "v0.32.0-rc.1"
+	DEV_PLATFORM        = "linux/amd64"
+	DEV_VERSION         = "dev"
+)
+
+var (
+	TRIVY_VERSION_NO_PREFIX    = strings.TrimPrefix(TRIVYVERSION, "v")
+	TRIVY_DOWNLOAD_URL         = "https://github.com/aquasecurity/trivy/releases/download/" + TRIVYVERSION + "/trivy_" + TRIVY_VERSION_NO_PREFIX + "_Linux-64bit.tar.gz"
+	TRIVY_ADAPTER_DOWNLOAD_URL = "https://github.com/goharbor/harbor-scanner-trivy/archive/refs/tags/" + TRIVYADAPTERVERSION + ".tar.gz"
+)
--- .dagger/main.go
+++ .dagger/main.go
@@ -8,33 +8,9 @@ import (
 	"strings"
 )
 
-// to-do: dagger dev
 // to-do: update registry to v3
 // to-do: add documentation
 // to-do: stop usage of shell things. No shell spawning
-
-const (
-	GOLANGCILINT_VERSION = "v1.61.0"
-	GO_VERSION           = "latest"
-	SYFT_VERSION         = "v1.9.0"
-	GORELEASER_VERSION   = "v2.3.2"
-	// version of registry for pulling the source code
-	REGISTRY_SRC_TAG = "v2.8.3"
-	// source of upstream distribution code
-	DISTRIBUTION_SRC = "https://github.com/distribution/distribution.git"
-	NPM_REGISTRY     = "https://registry.npmjs.org"
-	// trivy-adapter
-	TRIVYVERSION        = "v0.56.1"
-	TRIVYADAPTERVERSION = "v0.32.0-rc.1"
-	DEV_PLATFORM        = "linux/amd64"
-	DEV_VERSION         = "dev"
-)
-
-var (
-	TRIVY_VERSION_NO_PREFIX    = strings.TrimPrefix(TRIVYVERSION, "v")
-	TRIVY_DOWNLOAD_URL         = "https://github.com/aquasecurity/trivy/releases/download/" + TRIVYVERSION + "/trivy_" + TRIVY_VERSION_NO_PREFIX + "_Linux-64bit.tar.gz"
-	TRIVY_ADAPTER_DOWNLOAD_URL = "https://github.com/goharbor/harbor-scanner-trivy/archive/refs/tags/" + TRIVYADAPTERVERSION + ".tar.gz"
-)
 
 type (
 	Package  string
@@ -164,11 +140,11 @@ func (m *Harbor) PublishAllImages(
 		for _, imageTag := range imageTags {
 			container := dag.Container().WithRegistryAuth(registry, registryUsername, registryPassword)
 			imgAddress, err := container.Publish(ctx,
-				fmt.Sprintf("%s/%s/%s:%s", registry, "harbor", pkg, imageTag),
+				fmt.Sprintf("%s/%s/%s:%s", registry, "harbor-next", pkg, imageTag),
 				dagger.ContainerPublishOpts{PlatformVariants: imgs},
 			)
 			if err != nil {
-				fmt.Printf("Failed to publish image: %s/%s/%s:%s\n", registry, "harbor", pkg, imageTag)
+				fmt.Printf("Failed to publish image: %s/%s/%s:%s\n", registry, "harbor-next", pkg, imageTag)
 				fmt.Printf("Error: %s\n", err)
 				continue
 			}
