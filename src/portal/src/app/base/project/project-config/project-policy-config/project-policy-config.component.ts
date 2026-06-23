7d51353a780cb2dc23b8323ed9805b5064c8c8e7
Prasanth Baskar <bupdprasanth@gmail.com>
2026-06-23 09:29:31 +0000

    fix: Address upstream sync review feedback
    Signed-off-by: Prasanth Baskar <bupdprasanth@gmail.com>

--- src/portal/src/app/base/project/project-config/project-policy-config/project-policy-config.component.ts
+++ src/portal/src/app/base/project/project-config/project-policy-config/project-policy-config.component.ts
@@ -202,9 +202,6 @@ export class ProjectPolicyConfigComponent implements OnInit {
         this.retrieve();
         this.getPermission();
         this.getSystemAllowlist();
-        if (this.isSystemAdmin) {
-            this.getRegistries();
-        }
     }
 
     validateBandwidth(): void {
@@ -307,7 +304,10 @@ export class ProjectPolicyConfigComponent implements OnInit {
             .subscribe(permissins => {
                 this.hasChangeConfigRole = permissins as boolean;
                 this.allowUpdateProxyCacheConfiguration =
-                    this.hasChangeConfigRole && !this.isProxyCacheProject;
+                    this.hasChangeConfigRole;
+                if (this.allowUpdateProxyCacheConfiguration) {
+                    this.getRegistries();
+                }
             });
     }
 
