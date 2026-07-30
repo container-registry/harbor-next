6024f59fd0cb206482aa83bb1453ddd972a72c51
Prasanth Baskar <prasanth@8gears.com>
2026-07-30 18:01:03 +0530

    fix(deps): Remediate zero CVE findings (#467)
    (cherry picked from commit 871bef72c1e49735dcdb2c33ca9f6ff742ddebf2)
    Signed-off-by: Prasanth Baskar <prasanth@8gears.com>

--- src/pkg/reg/adapter/dtr/client.go
+++ src/pkg/reg/adapter/dtr/client.go
@@ -65,7 +65,7 @@ func (c *Client) getAndIteratePagination(endpoint string, v any) error {
 	}
 
 	rv := reflect.ValueOf(v)
-	if rv.Kind() != reflect.Ptr {
+	if rv.Kind() != reflect.Pointer {
 		return errors.New("v should be a pointer to a slice")
 	}
 	elemType := rv.Elem().Type()
@@ -307,7 +307,7 @@ func (c *Client) createRepository(repository string) error {
 // this operation needs admin access
 func (c *Client) createNamespace(namespace string) error {
 	ns := newDefaultDTRNamespace(namespace)
-	body, err := json.Marshal(ns)
+	body, err := json.Marshal(ns) // nolint:gosec // G117: DTR namespace creation requires the password field in its request body
 	if err != nil {
 		return err
 	}
